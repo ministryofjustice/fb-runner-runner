eb4ed1e535ef0c80c03df7c4525d0bec69805b98
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-11-09 16:27:11 +0000

    Move service folder outside of layouts

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -1,11 +1,5 @@
 class ServiceController < ApplicationController
   def start
-    service = Service.new
-  end
-
-  def next
-    datastore.save(params)
-    service = Service.new
-    render service.next_page
+    @service = Service.new
   end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -9,13 +9,18 @@ class Service
       "created_at": "2020-10-09T11:51:46",
       "created_by": "4634ec01-5618-45ec-a4e2-bb5aa587e751",
       "configuration": {
-      "_id": "service",
-      "_type": "config.service"
+        "_id": "service",
+        "_type": "config.service"
       },
       "pages": [
         {
-          "_id": "page.start",
-          "_type": "page.start",
+          "body": "You cannot use this form to complain about:\r\n\r\n* the result of a case\r\n* a judge, magistrate, coroner or member of a tribunal\r\n\r\nThis online form is also available in [Welsh (Cymraeg)](https://complain-about-a-court-or-tribunal.form.service.justice.gov.uk/cy).",
+          "heading": "Complain about a court or tribunal",
+          "lede": "Your complaint will not affect your case.",
+          "steps": [
+            "page.name",
+            "page.do-you-have-a-case-number"
+          ],
           "url": "/"
         }
       ],
